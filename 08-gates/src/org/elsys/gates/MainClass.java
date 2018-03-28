07cc504c79fec38413af3682b3bf4a620980c0b3
toniuyt123 <never_toni@abv.bg>
2018-03-28 19:28:51 +0300

    aide nashte

--- 08-gates/src/org/elsys/gates/MainClass.java
+++ 08-gates/src/org/elsys/gates/MainClass.java
@@ -1,7 +1,6 @@
 package org.elsys.gates;
 
 import java.util.Scanner;
-import org.elsys.gates.GateFactory.makeRippleCarryAdder;
 
 public class MainClass {
 	
@@ -10,19 +9,28 @@ public class MainClass {
 		Wire[] a = new Wire[8];
 		Wire[] b = new Wire[8];
 		Wire[] sum = new Wire[8];
+		Wire cIn = new Wire("cIn");
+		Wire cOut = new Wire("cOut");
+		
+		for (int i = 0; i < a.length; ++i) {
+			a[i] = new Wire("a" + i);
+			b[i] = new Wire("b" + i);
+			sum[i] = new Wire("sum" + i);
+		}
+		
+		CompositeGate rippleAdd = (CompositeGate)GateFactory.makeRippleCarryAdder(a, b, cIn, sum, cOut);
 		
 		while(in.hasNext()) {
 			int num1 = in.nextInt();
 			int num2 = in.nextInt();
-			
-			
+						
 			for (int i = 0; i < a.length; ++i) {
 				int bit = 1 << i;
 				a[i].setSignal((bit & num1) != 0);
 				b[i].setSignal((bit & num2) != 0);
 			}
 
-			CompositeGate rippleAdd = (CompositeGate)makeRippleCarryAdder(a, b, new Wire("cIn"), sum, new Wire("cOut"));
+			rippleAdd.act();
 			
 			int result = 0;
 			for (int i = 0; i < sum.length; ++i) {
@@ -34,5 +42,7 @@ public class MainClass {
 			
 			System.out.println(result);
 		}
+		
+		in.close();
 	}
 }
